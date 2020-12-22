05bb19f8c12470c8707e7aeff08ca2f8b5d4be7a
Ikraam <IbnDaanis@Gmail.com>
2020-12-22 03:55:57 +0000

    Updated Data

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -4,9 +4,13 @@ import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className='my-3 p-3 rounded' variant='light'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img
+          src={product.image}
+          variant='top'
+          style={{ borderRadius: '2px' }}
+        />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
@@ -24,7 +28,7 @@ const Product = ({ product }) => {
             }`}
           />
         </Card.Text>
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as='h3'>${product.price.toFixed(2)}</Card.Text>
       </Card.Body>
     </Card>
   )
