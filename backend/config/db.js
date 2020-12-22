e21e43e9581d90889b61c6f587edb0781c59753c
Ikraam <IbnDaanis@Gmail.com>
2020-12-22 16:49:44 +0000

    Fetching products from database

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     })
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.green.underline.bold
+    )
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold)
     process.exit(1)
