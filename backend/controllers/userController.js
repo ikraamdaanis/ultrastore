a05cd0b3f09e278bf05b22c1aa707a719f002f5c
Ikraam <IbnDaanis@Gmail.com>
2020-12-25 20:25:59 +0000

    Update profile endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -53,7 +53,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc user profile
+// @desc User profile
 // @route GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
@@ -71,4 +71,29 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, registerUser, getUserProfile }
+// @desc Update profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const { user } = req
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+    const updatedUser = await user.save()
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile }
