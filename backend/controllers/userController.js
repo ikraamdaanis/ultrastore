2af36f304c32c00f5d7b2ae7221d61dd9085e2d9
Ikraam <IbnDaanis@Gmail.com>
2020-12-26 20:26:10 +0000

    Admin middleware and get users endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -96,4 +96,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile }
+// @desc all users
+// @route GET /api/users
+// @access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
