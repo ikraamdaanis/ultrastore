25a785e8d6d83f90e591eeb888a8aad4b57c52fc
Ikraam <IbnDaanis@Gmail.com>
2020-12-26 20:44:29 +0000

    Admin user list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
           <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/' exact component={HomeScreen} />
         </Container>
       </main>
