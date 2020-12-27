de9bb77645d73180a4125688dc331cc1099e51ef
Ikraam <IbnDaanis@Gmail.com>
2020-12-27 13:51:51 +0000

    Edit product screen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -64,7 +64,7 @@ export const productDeleteReducer = (state = {}, action) => {
 export const productCreateReducer = (state = {}, action) => {
   switch (action.type) {
     case PRODUCT_CREATE_REQUEST:
-      return { loading: true }
+      return { ...state, loading: true }
     case PRODUCT_CREATE_SUCCESS:
       return { loading: false, success: true, product: action.payload }
     case PRODUCT_CREATE_FAIL:
