a5fbfbdb3a59be62d766446a02e216c547a8b1e7
Ikraam <IbnDaanis@Gmail.com>
2021-03-20 23:05:03 +0000

    ProductScreen

--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -1,5 +1,62 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button } from 'react-bootstrap'
+import { Rating } from '../components'
+import products from '../products'
 
-export const ProductScreen = () => {
-  return <div>Product</div>
+export const ProductScreen = ({ match }) => {
+  const product = products.find(product => product._id === match.params.id)
+
+  return (
+    <>
+      <Link className='btn btn-dark my-3' to='/'>
+        Back
+      </Link>
+      <Row>
+        <Col md={7}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={5}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews}${product.numReviews === 1 ? ` review` : ' reviews'}`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: £{product.price}</ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+
+            <ListGroup.Item>
+              <Row>
+                <Col>Price:</Col>
+                <Col>
+                  <strong>£{product.price}</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>Status:</Col>
+                <Col>
+                  <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                Add to Cart
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </>
+  )
 }
+
+ProductScreen.propTypes = { match: PropTypes.object }
