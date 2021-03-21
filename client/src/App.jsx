a44a9aef093db0da3ce277129933c54afc905cb5
Ikraam <IbnDaanis@Gmail.com>
2021-03-21 21:51:13 +0000

    ScrollToTop component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
-import { Footer, Header } from './components'
+import { Footer, Header, ScrollToTop } from './components'
 import { Container } from 'react-bootstrap'
 import { HomeScreen, ProductScreen } from './screens'
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <main className='py-3'>
         <Container>
