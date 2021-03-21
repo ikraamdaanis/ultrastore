6506d6e47f35afda8cc3c45617dda473c125d1e8
Ikraam <IbnDaanis@Gmail.com>
2021-03-21 22:44:41 +0000

    Made loader smaller

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -7,8 +7,8 @@ export const Loader = () => {
       animation='border'
       role='status'
       style={{
-        width: '100px',
-        height: '100px',
+        width: '40px',
+        height: '40px',
         margin: 'auto',
         display: 'block',
       }}
