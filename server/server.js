16ad4751efbeb0a94071f97e00617d14f326c39a
Ikraam <IbnDaanis@Gmail.com>
2021-03-22 18:20:16 +0000

    user authentication endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectToDatabase } from './config/database.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -10,14 +11,16 @@ connectToDatabase()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
