3786cba596a76e8ffa5f3febf9d36249bfcb5a8c
Ikraam <IbnDaanis@Gmail.com>
2021-03-22 18:32:13 +0000

    JSON web token

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken'
 
-export const generateToken = id => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+export const generateToken = id =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   })
-}
