a524143e70fe4b4d2296349d13ea1b584157504c
Ikraam <IbnDaanis@Gmail.com>
2021-03-22 22:25:11 +0000

    Login reducer and action

---
+++ client/src/redux/actions/userActions.js
@@ -0,0 +1,36 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from '../constants/userConstants'
+import axios from 'axios'
+
+export const userLogin = (email, password) => async dispatch => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST })
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    const { data } = await axios.post('/users/login', { email, password }, config)
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message ? error.response.data.message : error.message,
+    })
+  }
+}
+
+export const userLogout = () => dispatch => {
+  dispatch({ type: USER_LOGOUT })
+  localStorage.removeItem('userInfo')
+}
--- client/src/redux/reducers/userReducers.js
+++ client/src/redux/reducers/userReducers.js
@@ -1,5 +1,20 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from '../constants/userConstants'
+
 export const userLoginReducer = (state = {}, { type, payload }) => {
   switch (type) {
+    case USER_LOGIN_REQUEST:
+      return { loading: true }
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: payload }
+    case USER_LOGIN_FAIL:
+      return { loading: false, error: payload }
+    case USER_LOGOUT:
+      return {}
     default:
       return state
   }
