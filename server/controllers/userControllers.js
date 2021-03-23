f9e4b65d70f065eea2ceabe6b7d998f8055d2ba6
Ikraam <IbnDaanis@Gmail.com>
2021-03-23 01:54:36 +0000

    Update profile endpoint

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -55,13 +55,41 @@ export const registerUser = asyncHandler(async (req, res) => {
 // @route GET api/users/profile
 // @access Private
 export const getUserProfile = asyncHandler(async (req, res) => {
-  if (req.user) {
+  if (user) {
     const { user } = req
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(401)
+    throw new Error('User not found.')
+  }
+})
+
+// @desc Update user profile
+// @route PUT api/users/profile
+// @access Private
+export const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
     })
   } else {
     res.status(401)
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser } from '../controllers/userControllers.js'
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userControllers.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/', registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router
