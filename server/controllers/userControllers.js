7b19540886398290459408009112036d3d3816a7
Ikraam <IbnDaanis@Gmail.com>
2021-03-24 19:05:16 +0000

    Admin middleware, get users

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -96,3 +96,11 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found.')
   }
 })
+
+// @desc Get all users
+// @route GET api/users
+// @access Private/Admin
+export const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,14 +2,15 @@ import express from 'express'
 import {
   authUser,
   getUserProfile,
+  getUsers,
   registerUser,
   updateUserProfile,
 } from '../controllers/userControllers.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { admin, protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.post('/', registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
