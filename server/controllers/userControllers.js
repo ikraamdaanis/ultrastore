ed8ac39f8a509782050f1cb9879bdf28acb93602
Ikraam <IbnDaanis@Gmail.com>
2021-03-24 20:33:10 +0000

    Get user by id & update user

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -119,3 +119,42 @@ export const deleteUser = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
+
+// @desc Get user by ID
+// @route GET api/users/:id
+// @access Private/Admin
+export const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// @desc Update user
+// @route PUT api/users/:id
+// @access Private/Admin
+export const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin || user.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(401)
+    throw new Error('User not found.')
+  }
+})
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,9 +2,11 @@ import express from 'express'
 import {
   authUser,
   deleteUser,
+  getUserById,
   getUserProfile,
   getUsers,
   registerUser,
+  updateUser,
   updateUserProfile,
 } from '../controllers/userControllers.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
@@ -14,6 +16,10 @@ const router = express.Router()
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 export default router
