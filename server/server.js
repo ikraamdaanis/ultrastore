59f5ac849b727559172e005ad6c6edc8f457ba14
Ikraam <IbnDaanis@Gmail.com>
2021-03-25 11:57:25 +0000

    Image upload

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import { connectToDatabase } from './config/database.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
-import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 
@@ -21,8 +23,12 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/uploads', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
