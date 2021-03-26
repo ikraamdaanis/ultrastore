dd72552be8726db505395cde54446bef4c9811db
Ikraam <IbnDaanis@Gmail.com>
2021-03-26 22:18:03 +0000

    preparing for deployment

--- server/server.js
+++ server/server.js
@@ -19,10 +19,6 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -32,6 +28,15 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
